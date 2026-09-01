21492f8915295831b7ad707d70bcbe8a58fc354f
مقصودیVahid <p09052100734@gmail.com>
2026-09-02 03:02:51 +0330

    PHASE 03: import accounting.css in main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./styles/home.css";
 import "./styles/app.css";
+import "./styles/accounting.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
