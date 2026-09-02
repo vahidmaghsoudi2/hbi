02c184e1af2789c26b6e7e1f330df14fd154813f
مقصودیVahid <p09052100734@gmail.com>
2026-09-02 12:29:33 +0330

    PHASE 09: export payments router

--- app/api/routers/__init__.py
+++ app/api/routers/__init__.py
@@ -6,8 +6,10 @@ from app.api.routers.recommendations import router as recommendations_router
 from app.api.routers.inventory import router as inventory_router
 from app.api.routers.sales import router as sales_router
 from app.api.routers.evidence import router as evidence_router
+from app.api.routers.payments import router as payments_router
 
 __all__ = [
     "auth_router", "products_router", "customers_router", "cases_router",
     "recommendations_router", "inventory_router", "sales_router", "evidence_router",
+    "payments_router",
 ]
