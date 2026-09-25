9ec7fc378e5ec5c9e36d78f1787dfba8b2a54162
مقصودیVahid <p09052100734@gmail.com>
2026-09-25 11:48:04 +0330

    register FollowUp router

--- app/api/routers/__init__.py
+++ app/api/routers/__init__.py
@@ -11,10 +11,11 @@ from app.api.routers.returns import router as returns_router
 from app.api.routers.fx import router as fx_router
 from app.api.routers.reports import router as reports_router
 from app.api.routers.specialist import router as specialist_router
+from app.api.routers.followups import router as followups_router
 
 __all__ = [
     "auth_router", "products_router", "customers_router", "cases_router",
     "recommendations_router", "inventory_router", "sales_router", "evidence_router",
     "payments_router", "returns_router", "fx_router", "reports_router",
-    "specialist_router",
+    "specialist_router", "followups_router",
 ]
